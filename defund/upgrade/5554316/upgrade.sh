11a9c88aa40d8531cc911ee4bf9b1e6ba808378c
Indonode <34649601+elangrr@users.noreply.github.com>
2022-12-16 17:34:45 +0700

    Update upgrade.sh

--- defund/upgrade/5554316/upgrade.sh
+++ defund/upgrade/5554316/upgrade.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
-GREEN_COLOR='\033[0;32m'
-RED_COLOR='\033[0;31m'
-NO_COLOR='\033[0m'
+
+update_node() {
+  sudo systemctl stop defundd
+  cd $HOME && rm -rf defund
+  git clone https://github.com/defund-labs/defund
+  cd defund
+  git checkout $VERSION
+  make install
+  sudo systemctl restart defundd && journalctl -fu defundd -o cat
+}
+
 BLOCK=5554316
 VERSION=v0.2.0
-echo -e "$GREEN_COLOR YOUR NODE WILL BE UPDATED TO VERSION: $VERSION ON BLOCK NUMBER: $BLOCK $NO_COLOR\n"
-for((;;)); do
-	height=$(defundd status --node tcp://localhost:26657 |& jq -r ."SyncInfo"."latest_block_height")
-	if ((height>=$BLOCK)); then
 
-		sudo systemctl stop defundd
-		cd $HOME && rm -rf defund
-		git clone https://github.com/defund-labs/defund
-		cd defund
-		git checkout $VERSION
-		make install
-		sudo systemctl restart defundd && journalctl -fu defundd -o cat
+echo "Your node will be updated to version $VERSION on block number $BLOCK"
 
-		for (( timer=60; timer>0; timer-- )); do
-			printf "* second restart after sleep for ${RED_COLOR}%02d${NO_COLOR} sec\r" $timer
-			sleep 1
-		done
-		height=$(defundd status --node tcp://localhost:26657 |& jq -r ."SyncInfo"."latest_block_height")
-		if ((height>$BLOCK)); then
-			echo -e "$GREEN_COLOR YOUR NODE WAS SUCCESFULLY UPDATED TO VERSION: $VERSION $NO_COLOR\n"
-		fi
-		defundd version --long | head
-		break
-	else
-		echo -e "${GREEN_COLOR}$height${NO_COLOR} ($(( BLOCK - height  )) blocks left)"
-	fi
-	sleep 5
+while true; do
+  height=$(defundd status --node tcp://localhost:26657 | jq -r .SyncInfo.latest_block_height || echo 0)
+
+  if ((height >= BLOCK)); then
+    update_node && sleep 60
+    height=$(defundd status --node tcp://localhost:26657 | jq -r .SyncInfo.latest_block_height || echo 0)
+    if ((height > BLOCK)); then
+      echo "Your node was successfully updated to version $VERSION"
+    fi
+    defundd version --long | head
+    break
+  else
+    echo "$height ($(( BLOCK - height  )) blocks left)"
+  fi
+
+  sleep 5
 done
